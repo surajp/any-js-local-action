e847daed667c53d63dde55495eccd71388a41a29
Suraj Pillai <85.suraj@gmail.com>
2020-05-15 18:26:31 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Execute any JS in Flows with a single local action
+
+Convert strings in flows to arrays/collections, concat, use map, reduce, filters, and more..!.
+
+![](media/flow-anyaction-2.gif)
---
+++ force-app/main/default/aura/anyLocalAction/anyLocalActionController.js
@@ -0,0 +1,9 @@
+({
+  invoke: function (component, event, helper) {
+    let js = component.get("v.js");
+    let args = JSON.parse(component.get("v.args") || "{}");
+    let op = Function("args", `return ${js}`)(args);
+    if (Array.isArray(op)) component.set("v.outputArray", op);
+    else component.set("v.output", op);
+  }
+});
