430742eb3a45569dbbc4dc9b9c37dcc3dea02998
Suraj Pillai <85.suraj@gmail.com>
2020-05-18 19:13:38 -0400

    Tweak to allow multi-line js statements
    Explicit 'return' statement is required.

--- force-app/main/default/aura/anyLocalAction/anyLocalActionController.js
+++ force-app/main/default/aura/anyLocalAction/anyLocalActionController.js
@@ -2,7 +2,7 @@
   invoke: function (component, event, helper) {
     let js = component.get("v.js");
     let args = JSON.parse(component.get("v.args") || "{}");
-    let op = Function("args", `return ${js}`)(args);
+    let op = Function("args", `${js}`)(args);
     if (Array.isArray(op)) component.set("v.outputArray", op);
     else component.set("v.output", op);
   }
