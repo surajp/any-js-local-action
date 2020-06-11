aad043398802e342be4e306f5c9f20cbebc17e31
Suraj Pillai <85.suraj@gmail.com>
2020-06-11 11:48:15 -0400

    Add error handling

--- force-app/main/default/aura/anyLocalAction/anyLocalActionController.js
+++ force-app/main/default/aura/anyLocalAction/anyLocalActionController.js
@@ -1,9 +1,21 @@
 ({
-  invoke: function (component, event, helper) {
+  invoke: function (component) {
     let js = component.get("v.js");
-    let args = JSON.parse(component.get("v.args") || "{}");
-    let op = Function("args", `${js}`)(args);
-    if (Array.isArray(op)) component.set("v.outputArray", op);
-    else component.set("v.output", op);
+    let showError = component.get("v.showErrorToast");
+    try {
+      let args = JSON.parse(component.get("v.args") || "{}");
+      let op = Function("args", `${js}`)(args);
+      if (Array.isArray(op)) component.set("v.outputArray", op);
+      else component.set("v.output", op);
+    } catch (err) {
+      console.error(err);
+      if (showError) {
+        //eslint-disable-next-line
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({ title: "Error!", message: err.message });
+        toastEvent.fire();
+      }
+      throw err;
+    }
   }
 });
